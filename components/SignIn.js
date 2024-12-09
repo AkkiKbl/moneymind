e49a5d270e787377f4cbe90a1b2b19e975929eab
Akshay Kubal <akshay2298060@gmail.com>
2024-12-09 20:24:27 +0530

    first commit

---
+++ components/SignIn.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+
+import { authContext } from "@/lib/store/auth-context";
+
+import { FcGoogle } from "react-icons/fc";
+
+function SignIn() {
+  const { googleLoginHandler } = useContext(authContext);
+
+  return (
+    <main className="container max-w-2xl px-6 mx-auto">
+      <h1 className="mb-6 text-6xl font-bold text-center">
+        Welcome to MoneyMind
+      </h1>
+
+      <div className="flex flex-col overflow-hidden shadow-md shadow-slate-500 bg-slate-800 rounded-2xl">
+        <div className="px-4 py-6 mt-5">
+          <h3 className="text-2xl text-center">Sign-In To Continue</h3>
+
+          <button
+            onClick={googleLoginHandler}
+            className="flex self-start gap-2 p-4 mx-auto mt-6 font-medium text-white align-middle bg-gray-700 rounded-lg"
+          >
+            <FcGoogle className="text-2xl" /> Google
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default SignIn;
